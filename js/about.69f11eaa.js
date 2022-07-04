96f66be5a93241ea62f6cd075252824607a9b9a6
AlanSan Martín <a.sanmartin.correa@gmail.com>
2022-07-04 19:32:16 +0000

    "gh-pages"

---
+++ js/about.69f11eaa.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkportafoliovue"]=self["webpackChunkportafoliovue"]||[]).push([[443],{7381:function(n,u,t){t.r(u),t.d(u,{default:function(){return l}});var a=t(3396);const e={class:"about"},o=(0,a._)("h1",null,"This is an about page",-1),r=[o];function s(n,u){return(0,a.wg)(),(0,a.iD)("div",e,r)}var i=t(89);const c={},f=(0,i.Z)(c,[["render",s]]);var l=f}}]);
+//# sourceMappingURL=about.69f11eaa.js.map
